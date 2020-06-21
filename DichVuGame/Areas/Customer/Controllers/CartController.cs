8b1a119d765e39609fe7ab618d408a9b4d0c06f1
Ngoc Thanh <yasuo12091999@gmail.com>
2020-06-21 07:58:58 +0700

    1

---
+++ DichVuGame/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DichVuGame.Data;
+using DichVuGame.Extensions;
+using DichVuGame.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DichVuGame.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index(int? id)
+        {
+            List<CartViewModel> lstCart = HttpContext.Session.Get<List<CartViewModel>>("ShoppingCartSession");
+            if (lstCart == null)
+            {
+                lstCart = new List<CartViewModel>();
+            }
+            if (id.HasValue)
+            {
+                bool alreadyInCart = false;
+                var game = await _db.Games.Where(u => u.ID == id).FirstOrDefaultAsync();
+                foreach (var item in lstCart)
+                {
+                    if (item.Game.ID == id)
+                    {
+                        item.Amount++;
+                        alreadyInCart = true;
+                        break;
+                    }
+                }
+                if (alreadyInCart == false)
+                {
+                    lstCart.Add(new CartViewModel()
+                    {
+                        Game = game,
+                        Amount = 1,
+                    });
+                }
+            }
+            HttpContext.Session.Set("ShoppingCartSession", lstCart);
+            return View(lstCart);
+        }
+        public IActionResult RemoveFromCartInHome(int id)
+        {
+            List<CartViewModel> lstCart = HttpContext.Session.Get<List<CartViewModel>>("ShoppingCartSession");
+            if (lstCart == null)
+            {
+                lstCart = new List<CartViewModel>();
+            }
+            foreach (var item in lstCart)
+            {
+                if (item.Game.ID == id)
+                {
+                    lstCart.Remove(item);
+                    break;
+                }
+            }
+            HttpContext.Session.Set("ShoppingCartSession", lstCart);
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult RemoveFromCartInCart(int id)
+        {
+            List<CartViewModel> lstCart = HttpContext.Session.Get<List<CartViewModel>>("ShoppingcartSession");
+            if(lstCart == null)
+            {
+                lstCart = new List<CartViewModel>();
+            }
+            foreach(var item in lstCart)
+            {
+                if(item.Game.ID == id)
+                {
+                    lstCart.Remove(item);
+                    break;
+                }
+            }
+            HttpContext.Session.Set("ShoppingCartSession", lstCart);
+            return View(nameof(Index), lstCart);
+        }
+    }
+}
---
+++ DichVuGame/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DichVuGame.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public Game Game { get; set; }
+        public int Amount { get; set; }
+        public List<Game> Games { get; set; }
+        public GameAccount GameAccount { get; set; }
+    }
+}
